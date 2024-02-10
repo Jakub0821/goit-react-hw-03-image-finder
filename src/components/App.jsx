e72989f5305067d64b993e0cfffda26710af547c
Jakub0821 <jakubmigda120@gmail.com>
2024-02-11 00:46:56 +0100

    change index.js and App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
-import Searchbar from './SearchBar/SearchBar'
-import ImageGallery from './ImageGallery/ImageGallery'
+import Searchbar  from './Searchbar/Searchbar'
+import ImageGallery from './components/ImageGallery/ImageGallery'
 
 export class App extends Component {
     state = {
